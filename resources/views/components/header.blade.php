86df8b0d464152596d7ba1da04b5b9aa7bf06581
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2022-11-15 18:34:27 +0600

    first commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,20 @@
+<div class="fixed w-full py-4 px-12 flex justify-between items-center z-30">
+    <div class="min-w-max">
+        <img width="130px" class="object-none w-52 h-52 rounded-3xl custom-position" src="logo.png" alt="">
+        </div>
+        <div class="w-full">
+            <ul class="flex justify-center">
+                <li><a class="inline-block p-4 text-white text-4xl" href="">Vila</a></li>
+                <li><a class="inline-block p-4 text-white text-4xl" href="">Land</a></li>
+                <li><a class="inline-block p-4 text-white text-4xl" href="">Apartment</a></li>
+                <li><a class="inline-block p-4 text-white text-4xl" href="">About Us</a></li>
+                <li><a class="inline-block p-4 text-white text-4xl" href="">Contract</a></li>
+
+            </ul>
+            </div>
+        <div class="min-w-max text-3xl">
+            <a href="#" class="text-xs text-white">বাংলা</a>
+            <a href="#" class="text-xs text-white">English</a>
+        
+    </div>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<x-guest-layout>
+    <div class="relative z-10 pt-52 pb-64 bg-cover bg-center" style="background-image:url(bg.png)">
+    <div class="absolute h-full w-full bg-black opacity-70 top-0 left-0 z-10">
+
+    </div>
+    <div class="container relative z-20 text-white text-center text-2xl">
+        <h2 class="font-bold text-5xl" >Best choise for Renter and House-holder. </h2>
+        <p>Very easy to rent and find any house of your need.<br> We will give you all facilities for a renter and a holder to keep setisfactory level for every one.</p>
+
+    </div>
+    </div>
+</x-guest-layout>
